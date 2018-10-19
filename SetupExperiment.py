acfd9258f506ae3e04ac2a8dd81a980fcfd59f0b
sietse93 <sietsevschouwenburg@gmail.com>
2018-10-19 12:53:29 +0200

    Imported a CarlaGame Object

---
+++ SetupExperiment.py
@@ -0,0 +1,103 @@
+from __future__ import print_function
+from manual_control import CarlaGame, Timer
+
+import argparse
+import logging
+import random
+import time
+
+try:
+    import pygame
+    from pygame.locals import K_DOWN
+    from pygame.locals import K_LEFT
+    from pygame.locals import K_RIGHT
+    from pygame.locals import K_SPACE
+    from pygame.locals import K_UP
+    from pygame.locals import K_a
+    from pygame.locals import K_d
+    from pygame.locals import K_p
+    from pygame.locals import K_q
+    from pygame.locals import K_r
+    from pygame.locals import K_s
+    from pygame.locals import K_w
+except ImportError:
+    raise RuntimeError('cannot import pygame, make sure pygame package is installed')
+
+try:
+    import numpy as np
+except ImportError:
+    raise RuntimeError('cannot import numpy, make sure numpy package is installed')
+
+from carla import image_converter
+from carla import sensor
+from carla.client import make_carla_client, VehicleControl
+from carla.planner.map import CarlaMap
+from carla.settings import CarlaSettings
+from carla.tcp import TCPConnectionError
+from carla.util import print_over_same_line
+
+def main():
+    argparser = argparse.ArgumentParser(
+        description='CARLA Manual Control Client')
+    argparser.add_argument(
+        '-v', '--verbose',
+        action='store_true',
+        dest='debug',
+        help='print debug information')
+    argparser.add_argument(
+        '--host',
+        metavar='H',
+        default='localhost',
+        help='IP of the host server (default: localhost)')
+    argparser.add_argument(
+        '-p', '--port',
+        metavar='P',
+        default=2000,
+        type=int,
+        help='TCP port to listen to (default: 2000)')
+    argparser.add_argument(
+        '-a', '--autopilot',
+        action='store_true',
+        help='enable autopilot')
+    argparser.add_argument(
+        '-l', '--lidar',
+        action='store_true',
+        help='enable Lidar')
+    argparser.add_argument(
+        '-q', '--quality-level',
+        choices=['Low', 'Epic'],
+        type=lambda s: s.title(),
+        default='Low',
+        help='graphics quality level, a lower level makes the simulation run considerably faster')
+    argparser.add_argument(
+        '-m', '--map',
+        action='store_true',
+        help='plot the map of the current city')
+    args = argparser.parse_args()
+
+    log_level = logging.DEBUG if args.debug else logging.INFO
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=log_level)
+
+    logging.info('listening to server %s:%s', args.host, args.port)
+
+    print(__doc__)
+
+    while True:
+        try:
+
+            with make_carla_client(args.host, args.port) as client:
+                game = CarlaGame(client, args)
+                game.execute()
+                break
+
+        except TCPConnectionError as error:
+            logging.error(error)
+            time.sleep(1)
+
+
+if __name__ == '__main__':
+
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('\nCancelled by user. Bye!')
