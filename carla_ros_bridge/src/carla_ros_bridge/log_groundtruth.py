e135fdeaf5a273be77f809c19f2a3af118e7d7f6
Sietse93 <sietsevschouwenburg@gmail.com>
2018-11-23 10:33:11 +0100

    groundtruth logger logs txt files

--- carla_ros_bridge/src/carla_ros_bridge/log_groundtruth.py
+++ carla_ros_bridge/src/carla_ros_bridge/log_groundtruth.py
@@ -4,28 +4,54 @@ import rospy
 
 from tf2_msgs.msg import TFMessage
 
-def callback(data):
-    length = len(data.transforms)
-    for index in range(0, length-1):
+
+class CarlaGtLogger(object):
+
+    def __init__(self):
+        self.fname = rospy.get_param('rosbag_fname')
+        self.logger_layout = '%f   %f   %f  %f  %f  %f  %f  %f\n'
+        self.gt_logger = {}
+
+    def callback(self, data):
+        index = 0
+        time = data.transforms[index].header.stamp
+        game_stamp = time.to_sec()
         translation = data.transforms[index].transform.translation
         rotation = data.transforms[index].transform.rotation
-        rospy.loginfo("%f   %f  %f  %f  %f  %f  %f\n" %
-                      (translation.x,
-                       translation.y,
-                       translation.z,
-                       rotation.x,
-                       rotation.y,
-                       rotation.z,
-                       rotation.w))
-def logger():
+        log_string = (self.logger_layout % (game_stamp,
+                                            translation.x,
+                                            translation.y,
+                                            translation.z,
+                                            rotation.x,
+                                            rotation.y,
+                                            rotation.z,
+                                            rotation.w))
+        rospy.loginfo(log_string)
+        self.gt_logger.write(log_string)
 
-    rospy.init_node('GTLogger', anonymous=True)
+    def logger(self):
 
-    rospy.Subscriber("/tf", TFMessage, callback)
+        rospy.init_node('GTLogger', anonymous=True)
 
-    rospy.spin()
+        rospy.Subscriber("/tf", TFMessage, self.callback)
+
+        rospy.spin()
+
+    def __enter__(self):
+        self.gt_logger = open(self.fname + ".txt", "w")
+        rospy.loginfo("Gt file is open")
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.gt_logger.close()
+        rospy.loginfo("Gt file is closed")
+        return None
 
 
-if __name__ == '__main__':
+def main():
+    with CarlaGtLogger() as gt_log:
+        gt_log.logger()
 
-    logger()
+
+if __name__ == "__main__":
+    main()
