cf0ed1c1f26f81ea66f263c8560036984f0ead8e
Sietse93 <sietsevschouwenburg@gmail.com>
2018-12-04 14:30:49 +0100

    linear interpolation added

--- carla_ros_bridge/src/carla_ros_bridge/CarlaSlamPerformance.py
+++ carla_ros_bridge/src/carla_ros_bridge/CarlaSlamPerformance.py
@@ -3,7 +3,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import tf
-import math
+from pyquaternion import Quaternion
 
 
 class CarlaSlamEvaluate(object):
@@ -13,6 +13,7 @@ class CarlaSlamEvaluate(object):
         self.time = []
         self.position = []
         self.orientation = []
+        self.quaternion = []
         self.Q = []
         self.data = {}
         self.Q1Q2 = []
@@ -41,6 +42,7 @@ class CarlaSlamEvaluate(object):
                 else:
                     self.position.append(np.array(floatLine[1:4])-init_pos)
                 quaternions = (floatLine[4:8])
+                self.quaternion.append(quaternions)
                 self.orientation.append(tf.transformations.euler_from_quaternion(quaternions))  # roll, pitch, yaw
                 q = tf.transformations.quaternion_matrix(quaternions)
                 q[0][3] = floatLine[1]
@@ -61,6 +63,7 @@ class CarlaSlamEvaluate(object):
                 new_orientation = [orb_yaw, -orb_roll, -orb_pitch]
                 self.orientation.append(new_orientation)
                 quaternions = tf.transformations.quaternion_from_euler(new_orientation[0], new_orientation[1], new_orientation[2])
+                self.quaternion.append(quaternions)
                 q = tf.transformations.quaternion_matrix(quaternions)
                 q[0][3] = new_position[0]
                 q[1][3] = new_position[1]
@@ -141,35 +144,64 @@ def evaluate_pose_over_time(gt=CarlaSlamEvaluate, Slam = CarlaSlamEvaluate):
     plt.legend()
 
 
-def evaluate_PSE(gt = CarlaSlamEvaluate, Slam = CarlaSlamEvaluate):
+def evaluate_PSE(gt=CarlaSlamEvaluate, Slam=CarlaSlamEvaluate, time_step=float):
     RPE = []
+    time_debug = []
+    Q1Q2_debug = []
 
     for time in Slam.time:
-        if time > 2 and time < (Slam.time[-1]-1):
-            Q1 = Slam.Q[Slam.time.index(time)]
+        if time > 2 and time < (Slam.time[-1]-time_step):
+            time_index = Slam.time.index(time)
+            Q1 = Slam.Q[time_index]
             Q1_inv = np.linalg.inv(Q1)
 
             try:
-                Q2 = Slam.Q[Slam.time.index(time+1)]
-                Q1Q2_i = Q1_inv.dot(Q2)
-                Slam.Q1Q2.append(Q1Q2_i)
-                Slam.timeQ1Q2.append(time)
-
-                # get equivalent gt time index
-
-                gt_index = gt.time.index(time)
-                Q1_gt = gt.Q[gt_index]
-                Q1_gt_inv = np.linalg.inv(Q1_gt)
-                Q2_gt = gt.Q[gt.time.index(time+1)]
-                Q1Q2_gt_i = Q1_gt_inv.dot(Q2_gt)
-                gt.Q1Q2.append(Q1Q2_gt_i)
-                gt.timeQ1Q2.append(time)
-                Q1Q2_gt_i_inv = np.linalg.inv(Q1Q2_gt_i)
-                RPE_i = Q1Q2_gt_i_inv.dot(Q1Q2_i)
-                RPE.append(RPE_i)
+                Q2 = Slam.Q[Slam.time.index(time+time_step)]  # add time_step if it doesnt exist it gets a ValueError
 
             except ValueError:
-                pass
+                # if time step does not work, find the closest timestamp that is bigger than the time step
+                temp_index = time_index+1
+                time2 = Slam.time[temp_index]
+                while time2 < time + 1:
+                    temp_index = temp_index + 1
+                    time2 = Slam.time[temp_index]
+
+                #  use this data to linear interpolate the position
+                position1 = np.asarray(Slam.position[time_index])
+                position2 = np.asarray(Slam.position[temp_index])
+                position_inter = time_step*(position2 - position1)/(time2-time)+position1
+
+                # use Slerp technique to linear interpolate the rotations
+                q1 = Quaternion(Slam.quaternion[time_index])
+                q2 = Quaternion(Slam.quaternion[temp_index])
+                time_inter = time_step/(time2-time)
+                q = Quaternion.slerp(q1, q2, time_inter)
+                q_inter = [q[0], q[1], q[2], q[3]]
+                Q2 = tf.transformations.quaternion_matrix(q_inter)
+                Q2[0][3] = position_inter[0]
+                Q2[1][3] = position_inter[1]
+                Q2[2][3] = position_inter[2]
+
+                # some debug variables that allows you to visualize the linear interpolated data
+                time_debug.append(time)
+                Q1Q2_debug.append(Q1_inv.dot(Q2))
+
+            Q1Q2_i = Q1_inv.dot(Q2)
+            Slam.Q1Q2.append(Q1Q2_i)
+            Slam.timeQ1Q2.append(time)
+
+            # get equivalent gt time index
+
+            gt_index = gt.time.index(time)
+            Q1_gt = gt.Q[gt_index]
+            Q1_gt_inv = np.linalg.inv(Q1_gt)
+            Q2_gt = gt.Q[gt.time.index(time + time_step)]
+            Q1Q2_gt_i = Q1_gt_inv.dot(Q2_gt)
+            gt.Q1Q2.append(Q1Q2_gt_i)
+            gt.timeQ1Q2.append(time)
+            Q1Q2_gt_i_inv = np.linalg.inv(Q1Q2_gt_i)
+            RPE_i = Q1Q2_gt_i_inv.dot(Q1Q2_i)
+            RPE.append(RPE_i)
 
     RPEx = [matrix[0][3] for matrix in RPE]
     RPEy = [matrix[1][3] for matrix in RPE]
@@ -183,19 +215,25 @@ def evaluate_PSE(gt = CarlaSlamEvaluate, Slam = CarlaSlamEvaluate):
     Q1Q2gty = [matrix[1][3] for matrix in gt.Q1Q2]
     Q1Q2gtz = [matrix[2][3] for matrix in gt.Q1Q2]
 
+    Q1Q2_debugx = [matrix[0][3] for matrix in Q1Q2_debug]
+    Q1Q2_debugy = [matrix[1][3] for matrix in Q1Q2_debug]
+    Q1Q2_debugz = [matrix[2][3] for matrix in Q1Q2_debug]
+
     plt.figure("Q1Q2")
     plt.subplot(3, 1, 1)
     plt.plot(gt.timeQ1Q2, Q1Q2gtx, label=gt.method)
     plt.plot(Slam.timeQ1Q2, Q1Q2x, label=Slam.method)
+    # plt.plot(time_debug, Q1Q2_debugx, 'o', label='linear interpolated')
 
     plt.subplot(3, 1, 2)
     plt.plot(gt.timeQ1Q2, Q1Q2gty, label=gt.method)
     plt.plot(Slam.timeQ1Q2, Q1Q2y, label=Slam.method)
+    # plt.plot(time_debug, Q1Q2_debugy, 'o', label='linear interpolated')
 
     plt.subplot(3, 1, 3)
     plt.plot(gt.timeQ1Q2, Q1Q2gtz, label=gt.method)
     plt.plot(Slam.timeQ1Q2, Q1Q2z, label=Slam.method)
-
+    # plt.plot(time_debug, Q1Q2_debugz, 'o', label='linear interpolated')
 
     plt.figure("RPE")
     plt.subplot(3, 1, 1)
@@ -217,11 +255,13 @@ def main():
     with CarlaSlamEvaluate(method_orb, orb_file) as orb_data:
         orb_data.process_data()
 
+    time_step_evaluation = 1
+
     evaluate_trajectory(gt=gt_data, Slam=orb_data)
 
     evaluate_pose_over_time(gt=gt_data, Slam=orb_data)
 
-    evaluate_PSE(gt=gt_data, Slam=orb_data)
+    evaluate_PSE(gt=gt_data, Slam=orb_data, time_step=time_step_evaluation)
 
     plt.show()
 
