8b68a234b2065de703eb3a5b59a4454f13c655a7
Sietse93 <sietsevschouwenburg@gmail.com>
2019-04-09 11:02:20 +0200

    Does not make Rosbag when images are missing

--- ConvertImages2RosbagV2.py
+++ ConvertImages2RosbagV2.py
@@ -29,14 +29,59 @@ def GetFilesFromDir(dir):
                     elif 'right' in f or 'right' in path:
                         right_files.append( os.path.join( path, f ) )
                     all.append( os.path.join( path, f ) )
+
+    left_files_sorted = SortFiles(left_files)
+    right_files_sorted = SortFiles(right_files)
     all.sort()
-    left_files.sort()
-    right_files.sort()
-    return all, left_files, right_files
+
+    return all, left_files_sorted, right_files_sorted
+
+
+def SortFiles(file_list):
+    """sorting strings will results that /1000.png is sorted before /900.png.
+    This function sorts the strings based on integers"""
+
+    int_list = []
+    for file in file_list:
+        splitfile = file.split("/")
+
+        # select only the name of the image int.png
+        im_name = splitfile[-1]
+        # remove png
+        int_list.append(int(im_name[:-4]))
+    int_array = np.array(int_list)
+    sort_index = np.argsort(int_array).tolist()
+    sorted_files = [file_list[index] for index in sort_index]
+    return sorted_files
+
+
+def MissingImages(file_list):
+    """ Checks whether the sensor got all the images """
+
+    missing_files = []
+    int_list = []
+
+    # convert file_list to an integer list
+    for file in file_list:
+        file_name = file.split("/")[-1]
+        file_int = int(file_name[:-4])
+        int_list.append(file_int)
+
+    counter = int_list[0]
+
+    # checks if the list is a continuous stream of images
+    while counter != int_list[-1]:
+        try:
+            int_list.index(counter)
+        except ValueError:
+            missing_files.append(counter)
+        counter = counter + 1
+
+    return missing_files
 
 
 def CreateStereoBag(left_imgs, right_imgs, bagname):
-    '''Creates a bag file containing stereo image pairs'''
+    '''Creates a bag file containing stereo image pairs '''
     bag =rosbag.Bag(bagname, 'w')
     t = 0.0
     bridge = CvBridge()
@@ -51,6 +96,7 @@ def CreateStereoBag(left_imgs, right_imgs, bagname):
             img_cv_right = cv2.imread(right_imgs[i])
             Img_right = bridge.cv2_to_imgmsg(img_cv_right, encoding="rgb8")
 
+            # TO DO: IF IMAGE IS SKIPPED THAN THE HEADER TIME SHOULD BE DIFFERENT
             # fps=10
             t = t + 0.1
 
@@ -107,12 +153,26 @@ def CreateBag(args):
         print("No images found in %s" % args[0])
         exit()
 
-    if len(left_imgs) > 0 and len(right_imgs) > 0:
-        # create bagfile with stereo camera image pairs
-        CreateStereoBag(left_imgs, right_imgs, args[1])
+    # list of integers of the images that are missing
+    missing_im_left = MissingImages(left_imgs)
+    missing_im_right = MissingImages(right_imgs)
+
+    if len(missing_im_left) == 0 and len(missing_im_right == 0):
+        print("No missing images")
+
+        if len(left_imgs) > 0 and len(right_imgs) > 0:
+            # create bagfile with stereo camera image pairs
+            CreateStereoBag(left_imgs, right_imgs, args[1])
+        else:
+            # create bagfile with mono camera image stream
+            CreateMonoBag(all_imgs, args[1])
     else:
-        # create bagfile with mono camera image stream
-        CreateMonoBag(all_imgs, args[1])
+        print("Images are missing \n"
+              "Left images({}): {} \n"
+              "Right images({}): {} \n".format(len(missing_im_left), [im for im in missing_im_left],
+                                               len(missing_im_right), [im for im in missing_im_right]))
+
+
 
 
 if __name__ == "__main__":
