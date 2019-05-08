2800e5906f4abd56d10ba112cf38ee859dde363b
Sietse93 <sietsevschouwenburg@gmail.com>
2019-05-08 13:20:17 +0200

    add function that publishes results in latex table format

--- main_ScenarioLocationPerformance.py
+++ main_ScenarioLocationPerformance.py
@@ -12,9 +12,9 @@ def main():
     scenario_performance_data = []
     for Town in Towns:
         if Town == 1:
-            starting_locations = (27, 0, 58)
+            starting_locations = (0, 27, 58)
         elif Town == 2:
-            starting_locations = (37, 78)
+            starting_locations = (18, 37, 78)
         elif Town == 3:
             starting_locations = (75, 97, 127, 132)
         else:
@@ -29,8 +29,9 @@ def main():
             print("Converting T{}SL{}".format(Town, SL))
             Test = ScenarioLocationPerformance(ds, Town, SL, orb_static, orb_dynamic, gt)
             scenario_performance_data.append(Test)
-
     A = ScenarioPerformance(scenario_performance_data)
+    A.CreateLatexTable(scenario_performance_data)
+    A.SummaryPerformance()
     pdb.set_trace()
 
 
@@ -47,41 +48,92 @@ class ScenarioPerformance:
         total_false_loop_static = 0.0
         total_false_loop_dynamic = 0.0
 
+        total_static_vs_dynamic_trans = 0.0
+        total_static_vs_dynamic_rot = 0.0
+
         total_rmse_static_trans = 0.0
         total_rmse_static_rot = 0.0
 
         total_rmse_dynamic_trans = 0.0
         total_rmse_dynamic_rot = 0.0
 
+        nr_all_data_filtered = 0
         for data in list_scenario_performance:
             total_lost_track_static += data.lost_track_static
             total_false_loop_static += data.false_loop_static
             total_filtered_static += data.ratio_filtered_static
-            total_rmse_static_trans += data.rmse_static_avg[0]
-            total_rmse_static_rot += data.rmse_static_avg[1]
+            # Is it fair to have an unweighted average of the performance value, when not all performance value have
+            # the same amount of data points.
+            # total_rmse_static_trans += data.rmse_static_avg[0]
+            # total_rmse_static_rot += data.rmse_static_avg[1]
 
             total_lost_track_dynamic += data.lost_track_dynamic
             total_false_loop_dynamic += data.false_loop_dynamic
             total_filtered_dynamic += data.ratio_filtered_dynamic
-            total_rmse_dynamic_trans += data.rmse_dynamic_avg[0]
-            total_rmse_dynamic_rot += data.rmse_dynamic_avg[1]
+            # total_rmse_dynamic_trans += data.rmse_dynamic_avg[0]
+            # total_rmse_dynamic_rot += data.rmse_dynamic_avg[1]
+
+            if data.static_vs_dynamic_avg == None:
+                nr_all_data_filtered += 1
+            else:
+                total_static_vs_dynamic_trans += data.static_vs_dynamic_avg[0]
+                total_static_vs_dynamic_rot += data.static_vs_dynamic_avg[1]
 
         self.avg_filtered_static = total_filtered_static/len(list_scenario_performance)
         self.avg_false_loop_static = total_false_loop_static/len(list_scenario_performance)
         self.avg_lost_track_static = total_lost_track_static/len(list_scenario_performance)
-        self.avg_rmse_static = (total_rmse_static_trans/len(list_scenario_performance),
-                                total_rmse_static_rot/len(list_scenario_performance))
+        # self.avg_rmse_static = (total_rmse_static_trans/len(list_scenario_performance),
+        #                        total_rmse_static_rot/len(list_scenario_performance))
 
         self.avg_filtered_dynamic = total_filtered_dynamic/len(list_scenario_performance)
         self.avg_false_loop_dynamic = total_false_loop_dynamic/len(list_scenario_performance)
         self.avg_lost_track_dynamic = total_lost_track_dynamic/len(list_scenario_performance)
-        self.avg_rmse_dynamic = (total_rmse_dynamic_trans/len(list_scenario_performance),
-                                total_rmse_dynamic_rot/len(list_scenario_performance))
+        # self.avg_rmse_dynamic = (total_rmse_dynamic_trans/len(list_scenario_performance),
+        #                        total_rmse_dynamic_rot/len(list_scenario_performance))
 
-        stat_vs_dyn_trans = (self.avg_rmse_dynamic[0] - self.avg_rmse_static[0]) / self.avg_rmse_static[0] * 100.0
-        stat_vs_dyn_rot = (self.avg_rmse_dynamic[1] - self.avg_rmse_static[1]) / self.avg_rmse_static[1] * 100.0
-        self.avg_static_vs_dynamic = (stat_vs_dyn_trans, stat_vs_dyn_rot)
+        # take the average of increase error, but take into account the locations when all dynamic data is filtered out.
+        self.avg_static_vs_dynamic = (total_static_vs_dynamic_trans /
+                                      (len(list_scenario_performance)-nr_all_data_filtered),
+                                      total_static_vs_dynamic_rot /
+                                      (len(list_scenario_performance)-nr_all_data_filtered))
 
+    def CreateLatexTable(self, list_scenario_performance):
+        """Creates table for latex purposes"""
+
+        # general format of table
+        format_table = r"\begin{table}[h!]" + "\n" + r"\centering" + "\n" + r"\begin{tabular}{c c|c|c|c|c} \hline" + "\n"
+
+        # generate static table
+        title_static = r"\multicolumn{6}{c}{Static scenario}\\\hline\hline" + "\n"
+
+        # strings in column
+        string_columns = r" & Nr & RMSE RPE trans. [-] & RMSE RPE rot. [deg/m] & tracking failure [\%] & false loop closure [\%] \\\hline" +"\n"
+
+        # static data town
+        table_content = ""
+        for i in range(10):
+            row_data = ""
+            if i == 0:
+                town_column = r"\multirow{3}{4em}{Town01}"
+            elif i == 3:
+                town_column = r"\multirow{3}{4em}{Town02}"
+            elif i == 6:
+                town_column = r"\multirow{3}{4em}{Town03}"
+            else:
+                town_column = ""
+            location_data = list_scenario_performance[i]
+            table_data = "& {} & {} & {} & {} & {}"
+            row_data = town_column + table_data.format(i, location_data.rmse_static_avg[0], location_data.rmse_static_avg[1],
+                              location_data.lost_track_static, location_data.false_loop_static) + r"\\" + "\n"
+            table_content += row_data
+        # row1 = r"\multirow{3}{4em}{Town01}" + " & 1 & {0:.4f} & {0:.4f} & {0:.1f} & {0:.1f} ".format(list_scenario_performance[0].rmse_static_avg[0],
+        #                                                                          list_scenario_performance[0].rmse_static_avg[1],
+        #                                                                          list_scenario_performance[0].lost_track_static,
+        #                                                                          list_scenario_performance[0].false_loop_static) + r"\\" + "\n"
+        # finish table
+        end_table = r"\end{tabular}" + "\n" + r"\caption{Caption}" + "\n" + r"\label{tab:my_label}" + "\n" + r"\end{table}"
+        final_table = format_table + title_static + string_columns + table_content + end_table
+        print(final_table)
 
     def SummaryPerformance(self):
         """Prints a summary of the performance of the scenario and location"""
@@ -93,21 +145,17 @@ class ScenarioPerformance:
               "Filtered out:                    {}\n"
               "Track failure:                   {}\n"
               "False loops:                     {}\n"
-              "Increase error wrt static [%]:   {}\n"
-              "RMSE RPE:                        {}\n".format(self.avg_filtered_dynamic,
+              "Increase error wrt static [%]:   {}\n".format(self.avg_filtered_dynamic,
                                                              self.avg_lost_track_dynamic,
                                                              self.avg_false_loop_dynamic,
-                                                             self.avg_static_vs_dynamic,
-                                                             self.avg_rmse_dynamic))
+                                                             self.avg_static_vs_dynamic))
         print("Static scenario\n"
               "----------------\n"
               "Filtered out:                    {}\n"
               "Track failure:                   {}\n"
-              "False loops:                     {}\n"
-              "RMSE RPE:                        {}\n".format(self.avg_filtered_static,
+              "False loops:                     {}\n".format(self.avg_filtered_static,
                                                              self.avg_lost_track_static,
-                                                             self.avg_false_loop_static,
-                                                             self.avg_rmse_static))
+                                                             self.avg_false_loop_static))
 
 
 
