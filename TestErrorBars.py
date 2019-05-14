005ba83706dff3c5b460b997296fef6723a57f97
Sietse93 <sietsevschouwenburg@gmail.com>
2019-05-14 13:43:14 +0200

    add rotational component to graph

--- TestErrorBars.py
+++ TestErrorBars.py
@@ -12,7 +12,7 @@ def main():
     Towns = (1,2)
 
     # for Town in Towns:
-    Town = 1
+    Town = 3
     if Town == 1:
         starting_locations = (0, 27, 58)
     elif Town == 2:
@@ -32,8 +32,10 @@ def main():
     x = []
 
     for index, SL in enumerate(starting_locations):
-        y_mean = []
-        y_std = []
+        avg_trans = []
+        std_trans = []
+        avg_rot = []
+        std_rot = []
         track_fail = []
         loop_fail = []
 
@@ -48,35 +50,49 @@ def main():
             if ds == 20:
                 x.extend(["T" + str(Perf.location["Town"]) + "Nr" + str(index+1) + " Static",
                           "T" + str(Perf.location["Town"]) + "Nr" + str(index+1) + " Dist:" + str(Perf.scenario["Distance"])])
-                y_mean.extend([Perf.rmse_static_avg[0], Perf.rmse_dynamic_avg[0]])
-                y_std.extend([Perf.rmse_static_std[0], Perf.rmse_dynamic_std[0]])
+                avg_trans.extend([Perf.rmse_static_avg[0], Perf.rmse_dynamic_avg[0]])
+                std_trans.extend([Perf.rmse_static_std[0], Perf.rmse_dynamic_std[0]])
+                avg_rot.extend([Perf.rmse_static_avg[1], Perf.rmse_dynamic_avg[1]])
+                std_rot.extend([Perf.rmse_static_std[1], Perf.rmse_dynamic_std[1]])
                 track_fail.extend([Perf.lost_track_static, Perf.lost_track_dynamic])
                 loop_fail.extend([Perf.false_loop_static, Perf.false_loop_dynamic])
 
             else:
                 x.append("T" + str(Perf.location["Town"]) + "Nr" + str(index+1) + " Dist:" + str(Perf.scenario["Distance"]))
-                y_mean.append(Perf.rmse_dynamic_avg[0])
-                y_std.append(Perf.rmse_dynamic_std[0])
+                avg_trans.append(Perf.rmse_dynamic_avg[0])
+                std_trans.append(Perf.rmse_dynamic_std[0])
+                avg_rot.append(Perf.rmse_dynamic_avg[1])
+                std_rot.append(Perf.rmse_dynamic_std[1])
                 track_fail.append(Perf.lost_track_dynamic)
                 loop_fail.append(Perf.false_loop_dynamic)
         # have to loop it otherwise you get the same color
-        plt.subplot(2, 1, 1)
+        plt.subplot(3, 1, 1)
         # plt.grid(True)
         label_location = "T" + str(Perf.location["Town"]) + "Nr" + str(index+1)
-        plt.errorbar(ind[i:i+(len(dynamic_scenarios)+1)], np.array(y_mean), np.array(y_std), label=label_location)
+        plt.errorbar(ind[i:i+(len(dynamic_scenarios)+1)], np.array(avg_trans), np.array(std_trans), label=label_location)
         plt.legend()
-        width = 0.35
 
-        plt.subplot(2, 1, 2)
+        plt.subplot(3, 1, 2)
+        label_location = "T" + str(Perf.location["Town"]) + "Nr" + str(index + 1)
+        plt.errorbar(ind[i:i + (len(dynamic_scenarios) + 1)], np.array(avg_rot), np.array(std_rot), label=label_location)
+        plt.legend()
+
+        width = 0.35
+        plt.subplot(3, 1, 3)
         plot_track = plt.bar(ind[i:i+(len(dynamic_scenarios)+1)], track_fail, width, color="r")
         plot_loop = plt.bar(ind[i:i+(len(dynamic_scenarios)+1)], loop_fail, width, bottom=track_fail, color="b")
         i = i + (len(dynamic_scenarios)+1)
-    plt.subplot(2, 1, 1)
+    plt.subplot(3, 1, 1)
     plt.xticks(ind, x)
     plt.xlim(right=N)
     plt.title("Root Mean Square Error Relative Pose Error translational component")
 
-    plt.subplot(2, 1, 2)
+    plt.subplot(3, 1, 2)
+    plt.xticks(ind, x)
+    plt.xlim(right=N)
+    plt.title("Root Mean Square Error Relative Pose Error rotational component")
+
+    plt.subplot(3, 1, 3)
     plt.title("Ratio removed due to false loop closure or tracking failure")
     plt.xticks(ind,x)
     plt.ylim(top=1.1)
