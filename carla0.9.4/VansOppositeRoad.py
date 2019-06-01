b3a6af88eca11fc4d61a22a9658abe720b855ceb
Sietse93 <sietsevschouwenburg@gmail.com>
2019-06-01 12:05:11 +0200

    allow static and dynamic scenario simulations

--- carla0.9.4/VansOppositeRoad.py
+++ carla0.9.4/VansOppositeRoad.py
@@ -28,13 +28,21 @@ sys.path.append('/home/sietse/carla/PythonAPI')
 
 
 def main():
-    SL = 78
+    Town = 1
+    SL = 58
+    nr_vans = 10
+    scenario = "dynamic"
+    run_simulation(Town, SL, scenario, nr_vans)
+
+def run_simulation(Town, SL, scenario, nr_vans):
     fps = 20.0
     speed = 15.0
     total_distance = 500
 
+
     v = speed / 3.6
     x_step = v * (1 / fps)
+    image_length = 680
 
     actor_list = []
 
@@ -89,117 +97,113 @@ def main():
             total_dist += math.sqrt(dtrav.x**2 + dtrav.y**2 + dtrav.z**2)
             hero_route.append(next_waypoint)
 
-        ## Now define the route on the opposite lane, based on hero's route
-        opposite_route = []
-
-        # get all waypoints from the map
-        map_waypoint_list = map.generate_waypoints(x_step)
-
-        # note: map_waypoint_list does not contain waypoints on intersections
-        indices_intersection = []
-        for index, hero_waypoint in enumerate(hero_route):
-            # print progress
-            print(float(index)/float(len(hero_route))*100.0)
-
-            # get relevant road information from hero waypoint
-            hero_loc = hero_waypoint.transform.location
-            hero_road_id = hero_waypoint.road_id
-            hero_lane_id = hero_waypoint.lane_id
-            opp_lane_id = hero_lane_id * -1
-
-            # initialize value for closest waypoint
-            closest_waypoint = map_waypoint_list[0]
-
-            # check whether the current hero waypoint is at the beginning of an intersection
-            if hero_waypoint.is_intersection is True:
-                indices_intersection.append(index)
-                closest_waypoint = 0
-            else:
-                # check for each hero waypoint the closest opposite waypoint in the generated waypoint list.
-                # THIS SHOULD BE MORE EFFICIENTLY PROGRAMMED
-                for map_waypoint in map_waypoint_list:
-                    map_road_id = map_waypoint.road_id
-                    map_lane_id = map_waypoint.lane_id
-                    map_loc = map_waypoint.transform.location
-                    # check if it is on the same road but on the opposite lane
-                    if map_road_id == hero_road_id and map_lane_id == opp_lane_id:
-                        ref_loc = closest_waypoint.transform.location
-                        d_map = hero_loc - map_loc
-                        d_ref = hero_loc - ref_loc
-
-                        map_dist = math.sqrt(d_map.x ** 2 + d_map.y ** 2 + d_map.z ** 2)
-                        ref_dist = math.sqrt(d_ref.x ** 2 + d_ref.y ** 2 + d_ref.z ** 2)
-
-                        if map_dist < ref_dist:
-                            closest_waypoint = map_waypoint
-            opposite_route.append(closest_waypoint)
-
-        # opposite_route contains zeros at intersections.
-        # find the start of the intersection on the opposite route
-        opposite_route_start_intersection_indices = [index for index, waypoint in enumerate(opposite_route) if opposite_route[index-1] == 0 and waypoint != 0]
-
-        # Opposite_route_start_intersection is an index that is a waypoint, but it is still an intersection.
-        # This means the road id is wrong. You need the next waypoint
-        # this for loop is to ensure when multiple intersections occur
-        print("COUNTERACT FOR INTERSECTIONS")
-        for start_intersection_index_og in opposite_route_start_intersection_indices:
-            # first ensure that although the hero_waypoint is not an intersection anymore, the opposite route is also
-            # not an intersection anymore. If it still is an intersection go to the next index
-            start_intersection_index = start_intersection_index_og
-            while opposite_route[start_intersection_index].is_intersection is True:
-                start_intersection_index = start_intersection_index_og + 1
-            # now start_intersection_index contains the first waypoint that is not an intersection on the opposite road.
-            # go down the list until both the hero as the opposite van are on the intersection
-            while start_intersection_index != start_intersection_index_og:
-                list_opposite_next_waypoint = opposite_route[start_intersection_index].next(x_step)
-                opposite_next_waypoint = choose_correct_waypoint(list_opposite_next_waypoint, hero_route,
-                                                                 start_intersection_index)
-                start_intersection_index -= 1
-                opposite_route[start_intersection_index] = opposite_next_waypoint
-
-            # Now we are at the point where the hero waypoint is off the intersection and we go back on the intersection
-            # for both hero and opposite car. These indices are stored in the indices_intersection list
-            opposite_intersection_index = start_intersection_index_og
-            opposite_next_waypoint_list = opposite_route[opposite_intersection_index].next(x_step)
-            opposite_intersection_index -= 1
-            opposite_next_waypoint = choose_correct_waypoint(opposite_next_waypoint_list, hero_route,
-                                                             opposite_intersection_index)
-            while opposite_intersection_index in indices_intersection:
-                opposite_route[opposite_intersection_index] = opposite_next_waypoint
-                opposite_intersection_index -= 1
-                opposite_next_waypoint_list = opposite_next_waypoint.next(x_step)
-                opposite_next_waypoint = choose_correct_waypoint(opposite_next_waypoint_list, hero_route,
-                                                                 opposite_intersection_index)
-
-        # now reverse the route so the vans starts at the other side of the route
-        opposite_route.reverse()
-
         # spawn the hero
         hero = world.spawn_actor(prius_bp, hero_spawn)
         actor_list.append(hero)
         hero.set_simulate_physics(False)
 
-        # spawn the vans in a dict construct
-        # NOTE:NOT ACTUALLY THE NUMBER OF VEHICLES
-        nr_vans = 8
-        van_dict = {}
-        van_index = 0
-        spawn_points = range(0, len(opposite_route), len(opposite_route)/(nr_vans-1))
-        for i in spawn_points:
-            van_transform = opposite_route[i].transform
-            van_transform.location.z += 2.0
-            van_key = "van{}".format(van_index)
-            van_route_index = "van{}_index".format(van_index)
-            van_dict[van_key] = world.spawn_actor(van_bp, van_transform)
-            van_dict[van_key].set_simulate_physics(False)
-            van_dict[van_route_index] = i
-            van_index += 1
+        ## Now define the route on the opposite lane, based on hero's route
 
-        # van_transform = opposite_route[0].transform
-        # van_transform.location.z += 2.0
-        # van = world.spawn_actor(van_bp, van_transform)
-        # actor_list.append(van)
-        # van.set_simulate_physics(False)
+        if scenario == "dynamic":
+            opposite_route = []
+
+            # get all waypoints from the map
+            map_waypoint_list = map.generate_waypoints(x_step)
+
+            # note: map_waypoint_list does not contain waypoints on intersections
+            indices_intersection = []
+            for index, hero_waypoint in enumerate(hero_route):
+                # print progress
+                print(float(index)/float(len(hero_route))*100.0)
+
+                # get relevant road information from hero waypoint
+                hero_loc = hero_waypoint.transform.location
+                hero_road_id = hero_waypoint.road_id
+                hero_lane_id = hero_waypoint.lane_id
+                opp_lane_id = hero_lane_id * -1
+
+                # initialize value for closest waypoint
+                closest_waypoint = map_waypoint_list[0]
+
+                # check whether the current hero waypoint is at the beginning of an intersection
+                if hero_waypoint.is_intersection is True:
+                    indices_intersection.append(index)
+                    closest_waypoint = 0
+                else:
+                    # check for each hero waypoint the closest opposite waypoint in the generated waypoint list.
+                    # THIS SHOULD BE MORE EFFICIENTLY PROGRAMMED
+                    for map_waypoint in map_waypoint_list:
+                        map_road_id = map_waypoint.road_id
+                        map_lane_id = map_waypoint.lane_id
+                        map_loc = map_waypoint.transform.location
+                        # check if it is on the same road but on the opposite lane
+                        if map_road_id == hero_road_id and map_lane_id == opp_lane_id:
+                            ref_loc = closest_waypoint.transform.location
+                            d_map = hero_loc - map_loc
+                            d_ref = hero_loc - ref_loc
+
+                            map_dist = math.sqrt(d_map.x ** 2 + d_map.y ** 2 + d_map.z ** 2)
+                            ref_dist = math.sqrt(d_ref.x ** 2 + d_ref.y ** 2 + d_ref.z ** 2)
+
+                            if map_dist < ref_dist:
+                                closest_waypoint = map_waypoint
+                opposite_route.append(closest_waypoint)
+
+            # opposite_route contains zeros at intersections.
+            # find the start of the intersection on the opposite route
+            opposite_route_start_intersection_indices = [index for index, waypoint in enumerate(opposite_route) if opposite_route[index-1] == 0 and waypoint != 0]
+
+            # Opposite_route_start_intersection is an index that is a waypoint, but it is still an intersection.
+            # This means the road id is wrong. You need the next waypoint
+            # this for loop is to ensure when multiple intersections occur
+            print("COUNTERACT FOR INTERSECTIONS")
+            for start_intersection_index_og in opposite_route_start_intersection_indices:
+                # first ensure that although the hero_waypoint is not an intersection anymore, the opposite route is also
+                # not an intersection anymore. If it still is an intersection go to the next index
+                start_intersection_index = start_intersection_index_og
+                while opposite_route[start_intersection_index].is_intersection is True:
+                    start_intersection_index = start_intersection_index_og + 1
+                # now start_intersection_index contains the first waypoint that is not an intersection on the opposite road.
+                # go down the list until both the hero as the opposite van are on the intersection
+                while start_intersection_index != start_intersection_index_og:
+                    list_opposite_next_waypoint = opposite_route[start_intersection_index].next(x_step)
+                    opposite_next_waypoint = choose_correct_waypoint(list_opposite_next_waypoint, hero_route,
+                                                                     start_intersection_index)
+                    start_intersection_index -= 1
+                    opposite_route[start_intersection_index] = opposite_next_waypoint
+
+                # Now we are at the point where the hero waypoint is off the intersection and we go back on the intersection
+                # for both hero and opposite car. These indices are stored in the indices_intersection list
+                opposite_intersection_index = start_intersection_index_og
+                opposite_next_waypoint_list = opposite_route[opposite_intersection_index].next(x_step)
+                opposite_intersection_index -= 1
+                opposite_next_waypoint = choose_correct_waypoint(opposite_next_waypoint_list, hero_route,
+                                                                 opposite_intersection_index)
+                while opposite_intersection_index in indices_intersection:
+                    opposite_route[opposite_intersection_index] = opposite_next_waypoint
+                    opposite_intersection_index -= 1
+                    opposite_next_waypoint_list = opposite_next_waypoint.next(x_step)
+                    opposite_next_waypoint = choose_correct_waypoint(opposite_next_waypoint_list, hero_route,
+                                                                     opposite_intersection_index)
+
+            # now reverse the route so the vans starts at the other side of the route
+            opposite_route.reverse()
+
+            # spawn the vans in a dict construct
+            # NOTE:NOT ACTUALLY THE NUMBER OF VEHICLES
+
+            van_dict = {}
+            van_index = 0
+            spawn_points = range(0, len(opposite_route), len(opposite_route)/(nr_vans-1))
+            for i in spawn_points:
+                van_transform = opposite_route[i].transform
+                van_transform.location.z += 2.0
+                van_key = "van{}".format(van_index)
+                van_route_index = "van{}_index".format(van_index)
+                van_dict[van_key] = world.spawn_actor(van_bp, van_transform)
+                van_dict[van_key].set_simulate_physics(False)
+                van_dict[van_route_index] = i
+                van_index += 1
 
         clock = pygame.time.Clock()
 
@@ -214,19 +218,20 @@ def main():
             next_trans = hero_route[hero_route_index+1].transform
             batch = [carla.command.ApplyTransform(hero.id, next_trans)]
 
-            for i in range(len(spawn_points)):
-                cur_index = van_dict["van{}_index".format(i)]
-                next_index = cur_index + 1
-                van = van_dict["van{}".format(i)]
-                if next_index > len(opposite_route)-1:
-                    # if the van is at the end of its route, set the vehicle at the beginning of the route
-                    van_dict["van{}_index".format(i)] = 0
-                    next_trans_van = opposite_route[0].transform
-                    batch += [carla.command.ApplyTransform(van.id, next_trans_van)]
-                else:
-                    next_trans_van = opposite_route[next_index].transform
-                    van_dict["van{}_index".format(i)] = next_index
-                    batch += [carla.command.ApplyTransform(van.id, next_trans_van)]
+            if scenario == "dynamic":
+                for i in range(len(spawn_points)):
+                    cur_index = van_dict["van{}_index".format(i)]
+                    next_index = cur_index + 1
+                    van = van_dict["van{}".format(i)]
+                    if next_index > len(opposite_route)-1:
+                        # if the van is at the end of its route, set the vehicle at the beginning of the route
+                        van_dict["van{}_index".format(i)] = 0
+                        next_trans_van = opposite_route[0].transform
+                        batch += [carla.command.ApplyTransform(van.id, next_trans_van)]
+                    else:
+                        next_trans_van = opposite_route[next_index].transform
+                        van_dict["van{}_index".format(i)] = next_index
+                        batch += [carla.command.ApplyTransform(van.id, next_trans_van)]
 
             hero_route_index += 1
 
@@ -238,9 +243,14 @@ def main():
         settings.synchronous_mode = False
         world.apply_settings(settings)
 
-        print("Destroy actor")
+        print("Destroy prius")
         for actor in actor_list:
             actor.destroy()
+        if scenario == "dynamic":
+            for i in range(nr_vans):
+                van = van_dict["van{}".format(i)]
+                van.destroy()
+
         print("Actors destroyed")
 
 
