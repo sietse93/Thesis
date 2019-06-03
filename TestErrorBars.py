2ca4e081bd073d1203c45952f285242e588c7421
Sietse93 <sietsevschouwenburg@gmail.com>
2019-06-03 13:55:43 +0200

    latest version error bars

--- TestErrorBars.py
+++ TestErrorBars.py
@@ -8,7 +8,7 @@ import pdb
 def main():
     """Test if error bars show the results instinctively"""
     base_dir = "/home/sietse/results_carla0.9/stuckbehindvan/20fps/"
-    dynamic_scenarios = (20, 15)
+    dynamic_scenarios = (20, 15, 10)
     Towns = (1,2)
 
     # for Town in Towns:
@@ -45,11 +45,10 @@ def main():
 
             orb_static, gt = InspectJsonFileInDir(Town, SL, base_dir, dir_name_stat)
             orb_dynamic, gt = InspectJsonFileInDir(Town, SL, base_dir, dir_name_dyn)
-            # orb_static, orb_dynamic = FilterOutliersFromOrb(orb_static, orb_dynamic, Town, SL, ds)
             Perf = ScenarioLocationPerformance(ds, Town, SL, orb_static, orb_dynamic, gt)
             if ds == 20:
-                x.extend(["T" + str(Perf.location["Town"]) + "Nr" + str(index+1) + " Static",
-                          "T" + str(Perf.location["Town"]) + "Nr" + str(index+1) + " Dist:" + str(Perf.scenario["Distance"])])
+                x.extend(["T" + str(Perf.location["Town"]) + "Nr" + str(index+1) + "\n" +" Static",
+                          "T" + str(Perf.location["Town"]) + "Nr" + str(index+1) +  "\n"+ " Dist:" + str(Perf.scenario["Distance"])])
                 avg_trans.extend([Perf.rmse_static_avg[0], Perf.rmse_dynamic_avg[0]])
                 std_trans.extend([Perf.rmse_static_std[0], Perf.rmse_dynamic_std[0]])
                 avg_rot.extend([Perf.rmse_static_avg[1], Perf.rmse_dynamic_avg[1]])
@@ -58,7 +57,7 @@ def main():
                 loop_fail.extend([Perf.false_loop_static, Perf.false_loop_dynamic])
 
             else:
-                x.append("T" + str(Perf.location["Town"]) + "Nr" + str(index+1) + " Dist:" + str(Perf.scenario["Distance"]))
+                x.append("T" + str(Perf.location["Town"]) + "Nr" + str(index+1) + "\n" + "Dist:" + str(Perf.scenario["Distance"]))
                 avg_trans.append(Perf.rmse_dynamic_avg[0])
                 std_trans.append(Perf.rmse_dynamic_std[0])
                 avg_rot.append(Perf.rmse_dynamic_avg[1])
@@ -70,6 +69,7 @@ def main():
         # plt.grid(True)
         label_location = "T" + str(Perf.location["Town"]) + "Nr" + str(index+1)
         plt.errorbar(ind[i:i+(len(dynamic_scenarios)+1)], np.array(avg_trans), np.array(std_trans), label=label_location)
+        plt.subplots_adjust(hspace=0.5)
         plt.legend()
 
         plt.subplot(3, 1, 2)
@@ -84,19 +84,25 @@ def main():
         i = i + (len(dynamic_scenarios)+1)
     plt.subplot(3, 1, 1)
     plt.xticks(ind, x)
+    if Town == 1:
+        plt.ylim(top=0.060)
     plt.xlim(right=N)
-    plt.title("Root Mean Square Error Relative Pose Error translational component")
+    plt.title("Translational component root mean square error of the relative pose error")
+    plt.ylabel("Trans. RMSE RPE [-]")
 
     plt.subplot(3, 1, 2)
     plt.xticks(ind, x)
+    if Town == 1:
+        plt.ylim(top=0.040)
     plt.xlim(right=N)
-    plt.title("Root Mean Square Error Relative Pose Error rotational component")
-
+    plt.title("Rotational component root mean square error relative pose error")
+    plt.ylabel("Rot. RMSE RPE [deg/m]")
     plt.subplot(3, 1, 3)
     plt.title("Ratio removed due to false loop closure or tracking failure")
     plt.xticks(ind,x)
     plt.ylim(top=1.1)
     plt.xlim(right=N)
+    plt.ylabel("Ratio removed of total data [-]")
     plt.legend((plot_track, plot_loop), ("tracking failure", "false loop closure"))
     plt.show()
 
